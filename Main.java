a4700587afa2797732c06dd59c78dba4d5bffd6a
kczerwonka00 <101180432+kczerwonka00@users.noreply.github.com>
2022-05-31 09:23:00 +0000

    4_1

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 import java.io.IOException;
 
 class WrongStudentName extends Exception { }
+class WrongStudentAge extends Exception { }
+class WrongBirthDate extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -21,8 +23,11 @@ class Main {
 
             } catch(WrongStudentName e) {
                 System.out.println("Błędne imie studenta!");
-            }
+            } catch(WrongStudentAge e){
+               System.out.println("Błędny wiek studenta!");
+            
         }
+       }   
     }
 
     public static int menu() {
@@ -44,10 +49,18 @@ class Main {
         return name;
     }
 
-    public static void exercise1() throws IOException, WrongStudentName {
+    public static int ReadAge() throws WrongStudentAge{
+      System.out.println("Podaj wiek: ");
+      int wiek=scan.nextInt();
+      if(wiek<0 || wiek>100)
+        throw new WrongStudentAge();
+      return wiek;
+      
+    }
+  
+    public static void exercise1() throws IOException, WrongStudentName, WrongStudentAge {
         var name = ReadName();
-        System.out.println("Podaj wiek: ");
-        var age = scan.nextInt();
+        var age = ReadAge();
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
         var date = scan.nextLine();
